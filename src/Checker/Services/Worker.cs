7ccb0252ca2442502f975b4471aed1d379e69a09
Wesley Van Koeckhoven <wvk@outlook.be>
2023-10-16 21:47:52 +0200

    save changes

--- src/Checker/Services/Worker.cs
+++ src/Checker/Services/Worker.cs
@@ -1,4 +1,6 @@
 ﻿using Wvk.Dotnet.UpdateCheck;
+using Wvk.Dotnet.UpdateCheck.Comparison;
+using Wvk.Dotnet.UpdateCheck.Models.General;
 
 namespace Checker.Services;
 
@@ -7,6 +9,7 @@ public class Worker : BackgroundService
     private readonly ILogger<Worker> _logger;
     private readonly IDotnetVersionRetrieval _client;
     private static readonly TimeSpan delay = TimeSpan.FromSeconds(10);
+    private readonly IEqualityComparer<ReleaseIndex> _comparer = new ReleaseIndexComparison();
 
     public Worker(ILogger<Worker> logger, IDotnetVersionRetrieval client)
     {
@@ -30,12 +33,86 @@ public class Worker : BackgroundService
         {
             var reply = await _client.GetDotnetReleases(stoppingToken) ?? throw new NullReferenceException("The reply was empty");
 
-            foreach (var version in reply.ReleasesIndex)
-                _logger.LogInformation("Discovered {ChannelVersion}", version.ChannelVersion);
+            ProcessNewReply(reply.ReleasesIndex);
         }
         catch (Exception ex)
         {
             _logger.LogError(ex, "Error retrieving versions");
         }
     }
+
+    private void ProcessNewReply(IEnumerable<ReleaseIndex> newestData)
+    {
+        bool changes = false;
+        var dict = newestData.ToDictionary(d => d.ChannelVersion, d => d);
+
+        foreach (var key in _current.Keys.Union(dict.Keys))
+        {
+            if (!dict.TryGetValue(key, out var @new))
+            {
+                _logger.LogInformation("Version {key} seems to be removed", key);
+                _current.Remove(key);
+                changes = true;
+            }
+            else if (!_current.TryGetValue(key, out var old))
+            {
+                _logger.LogInformation("Version {key} discovered!", key);
+                _current.Add(key, dict[key]);
+                changes = true;
+            }
+            else
+            {
+                if (_comparer.Equals(old, @new)) continue;
+
+                changes = true;
+
+                if (old.Security != @new.Security)
+                    _logger.LogInformation("Version {key}: {property} changed from {oldValue} to {newValue}",
+                        key, nameof(ReleaseIndex.Security), old.Security, @new.Security);
+
+                if (old.ReleasesJson != @new.ReleasesJson)
+                    _logger.LogInformation("Version {key}: {property} changed from {oldValue} to {newValue}",
+                        key, nameof(ReleaseIndex.ReleasesJson), old.ReleasesJson, @new.ReleasesJson);
+
+                if (old.EolDate != @new.EolDate)
+                    _logger.LogInformation("Version {key}: {property} changed from {oldValue} to {newValue}",
+                        key, nameof(ReleaseIndex.EolDate), old.EolDate, @new.EolDate);
+
+                if (old.LatestRelease != @new.LatestRelease)
+                    _logger.LogInformation("Version {key}: {property} changed from {oldValue} to {newValue}",
+                        key, nameof(ReleaseIndex.LatestRelease), old.LatestRelease, @new.LatestRelease);
+
+                if (old.LatestReleaseDate != @new.LatestReleaseDate)
+                    _logger.LogInformation("Version {key}: {property} changed from {oldValue} to {newValue}",
+                        key, nameof(ReleaseIndex.LatestReleaseDate), old.LatestReleaseDate, @new.LatestReleaseDate);
+
+                if (old.LatestRuntime != @new.LatestRuntime)
+                    _logger.LogInformation("Version {key}: {property} changed from {oldValue} to {newValue}",
+                        key, nameof(ReleaseIndex.LatestRuntime), old.LatestRuntime, @new.LatestRuntime);
+
+                if (old.LatestSdk != @new.LatestSdk)
+                    _logger.LogInformation("Version {key}: {property} changed from {oldValue} to {newValue}",
+                        key, nameof(ReleaseIndex.LatestSdk), old.LatestSdk, @new.LatestSdk);
+
+                if (old.Product != @new.Product)
+                    _logger.LogInformation("Version {key}: {property} changed from {oldValue} to {newValue}",
+                        key, nameof(ReleaseIndex.Product), old.Product, @new.Product);
+
+                if (old.ReleaseType != @new.ReleaseType)
+                    _logger.LogInformation("Version {key}: {property} changed from {oldValue} to {newValue}",
+                        key, nameof(ReleaseIndex.ReleaseType), old.ReleaseType, @new.ReleaseType);
+
+                if (old.SupportPhase != @new.SupportPhase)
+                    _logger.LogInformation("Version {key}: {property} changed from {oldValue} to {newValue}",
+                        key, nameof(ReleaseIndex.SupportPhase), old.SupportPhase, @new.SupportPhase);
+
+                _current[key] = @new;
+            }
+        }
+
+        if (!changes)
+            _logger.LogDebug($"No changes detected at this time");
+    }
+
+    private readonly Dictionary<string, ReleaseIndex> _current = new();
 }
---
+++ src/Core/Comparison/ReleaseIndex.cs
@@ -0,0 +1,50 @@
+﻿using Wvk.Dotnet.UpdateCheck.Models.General;
+
+namespace Wvk.Dotnet.UpdateCheck.Comparison;
+
+public class ReleaseIndexComparison : IEqualityComparer<ReleaseIndex>
+{
+    public bool Equals(ReleaseIndex? x, ReleaseIndex? y)
+    {
+        if (ReferenceEquals(x, y))
+            return true;
+
+        if (ReferenceEquals(x, null))
+            return false;
+
+        if (ReferenceEquals(y, null))
+            return false;
+
+        if (x.GetType() != y.GetType())
+            return false;
+
+        return x.ChannelVersion == y.ChannelVersion
+               && x.LatestRelease == y.LatestRelease
+               && x.LatestReleaseDate == y.LatestReleaseDate
+               && x.Security == y.Security
+               && x.LatestRuntime == y.LatestRuntime
+               && x.LatestSdk == y.LatestSdk
+               && x.Product == y.Product
+               && x.ReleaseType == y.ReleaseType
+               && x.SupportPhase == y.SupportPhase
+               && x.EolDate == y.EolDate
+               && x.ReleasesJson == y.ReleasesJson;
+    }
+
+    public int GetHashCode(ReleaseIndex obj)
+    {
+        var hashCode = new HashCode();
+        hashCode.Add(obj.ChannelVersion);
+        hashCode.Add(obj.LatestRelease);
+        hashCode.Add(obj.LatestReleaseDate);
+        hashCode.Add(obj.Security);
+        hashCode.Add(obj.LatestRuntime);
+        hashCode.Add(obj.LatestSdk);
+        hashCode.Add(obj.Product);
+        hashCode.Add(obj.ReleaseType);
+        hashCode.Add(obj.SupportPhase);
+        hashCode.Add(obj.EolDate);
+        hashCode.Add(obj.ReleasesJson);
+        return hashCode.ToHashCode();
+    }
+}
